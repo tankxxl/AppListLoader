55dec0cf8f34c04188d2326db2ad26d188e56981
rengangzai <tank2xxl@gmail.com>
2015-01-04 17:12:58 +0800

    a demo for LoaderManager

---
+++ app/src/main/java/com/umpay/applistloader/AppListAdapter.java
@@ -0,0 +1,53 @@
+package com.umpay.applistloader;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.umpay.applistloader.loader.AppEntry;
+
+import java.util.List;
+
+/**
+ * A custom ArrayAdapter used by the {@link com.umpay.applistloader.MainActivity.AppListFragment} to display the
+ * device's installed applications.
+ */
+public class AppListAdapter extends ArrayAdapter<AppEntry> {
+    private LayoutInflater mInflater;
+
+    public AppListAdapter(Context ctx) {
+        super(ctx, android.R.layout.simple_list_item_2);
+        mInflater = (LayoutInflater) ctx
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view;
+
+        if (convertView == null) {
+            view = mInflater.inflate(R.layout.list_item_icon_text, parent, false);
+        } else {
+            view = convertView;
+        }
+
+        AppEntry item = getItem(position);
+        ((ImageView) view.findViewById(R.id.icon)).setImageDrawable(item.getIcon());
+        ((TextView) view.findViewById(R.id.text)).setText(item.getLabel());
+
+        return view;
+    }
+
+    public void setData(List<AppEntry> data) {
+        clear();
+        if (data != null) {
+            for (int i = 0; i < data.size(); i++) {
+                add(data.get(i));
+            }
+        }
+    }
+}
